f543229784a237bd8ce6fc61366efb6500bc47c1
Paula Rivero <priveroh01@educantabria.es>
2026-06-06 10:43:26 +0200

    cambios comandos

--- backend/app/Console/Commands/RestrasarAlquileres.php
+++ backend/app/Console/Commands/RestrasarAlquileres.php
@@ -8,35 +8,15 @@ use Carbon\Carbon;
 
 class RestrasarAlquileres extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'alquileres:retrasar';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        /* obtiene los alquileres que no se hayan marcado como completados pero si 
-        se ha recogido el producto (estado recogido) y cuya fecha de recogida 
-        ha pasado (con margen de 24 horas) */
         $retrasados = Alquiler::where('estado', 'recogido')
             ->where('fecha_devolucion', '<', Carbon::today())
             ->get();
 
-        /* los alquileres se marcan automaticamente como retrasados 
-        el estado del producto seguirá siendo alquilado hasta que 
-        el admin cancele manualmente el alquiler o lo marque como completado */
         foreach ($retrasados as $alquiler) {
             $alquiler->update(['estado' => 'retrasado']);
         }
--- backend/routes/console.php
+++ backend/routes/console.php
@@ -4,18 +4,13 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-/* todos los dias se ejecutan los siguientes comandos: 
-1. Comando para expirar reservas cuya fecha de recogida ha pasado y no se hayan marcado como completadas
-2. Comando para expirar alquileres cuya fecha de recogida ha pasado y no se hayan marcado como completados
-3. Comando para retrasar alquileres cuya fecha de devolución ha pasado y el producto ya se ha recogido
-*/
-Schedule::command('reservas:expirar')->daily();
-Schedule::command('alquileres:expirar')->daily();
-Schedule::command('alquileres:retrasar')->daily();
+// Schedule::command('reservas:expirar')->daily();
+// Schedule::command('alquileres:expirar')->daily();
+// Schedule::command('alquileres:retrasar')->daily();
 
-Schedule::command('reservas:recordar-recogida')->daily();
-Schedule::command('alquileres:recordar-recogida')->daily();
-Schedule::command('alquileres:recordar-devolucion')->daily();
+// Schedule::command('reservas:recordar-recogida')->daily();
+// Schedule::command('alquileres:recordar-recogida')->daily();
+// Schedule::command('alquileres:recordar-devolucion')->daily();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
